c6e78f4b08e47d3b6087f210274272e938e14771
seonghooncho <imalex124@gmali.com>
2025-12-01 00:01:38 +0900

    alb 백엔드 방향 도메인으로 변경

--- public/js/config.js
+++ public/js/config.js
@@ -4,7 +4,7 @@ const isProd = window.location.hostname !== "localhost"
 
 export const config = {
   API_BASE_URL: isProd
-    ? "https://nginx.millons-io.store"   // 실제 IP로 변경
+    ? "https://api.millons-io.store"   // 실제 IP로 변경
     : "http://localhost:8080",
   APP_NAME: "Kaboocam",
 }
